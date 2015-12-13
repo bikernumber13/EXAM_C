b9f8fd071c4b43666a5a98fae8fc8bb5edffae22
root <root@localhost.localdomain>
2015-12-13 12:49:24 +0100

    a tester examshell

---
+++ brainfuck.c
@@ -0,0 +1,71 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+		write(1, &c, 1);
+}
+
+void	*ft_solve(char *str)
+{
+	int				i;
+	int				j;
+	unsigned char	*ptr;
+
+	i = 0;
+	j = 0;
+	if (!(ptr = (char *)malloc(sizeof(*ptr) * 2048)))
+		return (NULL);
+	while (i < 2048)
+		ptr[i++] = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '>')
+			ptr++;
+		else if (str[i] == '<')
+			ptr--;
+		else if (str[i] == '+')
+			(*ptr)++;
+		else if (str[i] == '-')
+			(*ptr)--;
+		else if (str[i] == '.')
+			ft_putchar(*ptr);
+		else if ((str[i] == '[') && ((*ptr) == 0))
+		{
+			j = 1;
+			while (j)
+			{
+				i++;
+				if (str[i] == ']')
+					j--;
+				else if (str[i] == '[')
+					j++;
+			}
+		}
+		else if (str[i] == ']')
+		{
+			j = 1;
+			while (j)
+			{
+				i--;
+				if (str[i] == '[')
+					j--;
+				else if (str[i] == ']')
+					j++;
+			}
+			i--;
+		}
+		i++;
+	}
+}
+
+int	main(int ac, char ** av)
+{
+	if (ac == 2)
+		ft_solve(av[1]);
+	else
+		ft_putchar('\n');
+	return (0);
+}
